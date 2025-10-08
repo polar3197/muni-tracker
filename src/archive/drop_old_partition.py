19b744789bb1f91da3f883213afb0a7865737ab2
Your Name <charlie.c@berkeley.edu>
2025-10-07 23:11:13 -0700

    l

--- src/archive/drop_old_partition.py
+++ src/archive/drop_old_partition.py
@@ -1,7 +1,13 @@
 from src.database.db_manager import DatabaseManager
+from sqlalchemy import create_engine
 
 def main():
     # this will be run once a week. Because of this, it should 
+
+    # 1. connect to database
+    dbm = DatabaseManager()
+
+    dbm.exportOldestPartitionTos3()
     
     # 1. query to find oldest parition
 
@@ -11,8 +17,6 @@ def main():
     # 3. send the data over boto3 to the appropriate s3 bucker
 
     # 4. drop the partition
-
-    print(f"Successfully exported partition {year}-{week} to S3")
 
     return
 
