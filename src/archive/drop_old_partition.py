5cc748024db1846db5f942c9e9ce7e8aa3cf174a
Charlie Cooper <charliepeople427@gmail.com>
2025-10-08 06:22:56 +0000

    j

--- src/archive/drop_old_partition.py
+++ src/archive/drop_old_partition.py
@@ -2,10 +2,13 @@ from src.database.db_manager import DatabaseManager
 from sqlalchemy import create_engine
 
 def main():
+    db_url = f"postgresql://{os.getenv("POSTGRES_USERNAME")}:{os.getenv("POSTGRES_PASSWORD")}@{os.getenv("POSTGRES_HOST")}:5432/{os.getenv("POSTGRES_DB")}"
+
+    s3_bucket = os.getenv("charlie-muni-pg-backup")
     # this will be run once a week. Because of this, it should 
 
     # 1. connect to database
-    dbm = DatabaseManager()
+    dbm = DatabaseManager(db_url, s3_bucket)
 
     dbm.exportOldestPartitionTos3()
     
